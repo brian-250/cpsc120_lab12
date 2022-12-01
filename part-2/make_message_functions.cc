15078baabb641ae18c26a241f96a0e7b9b856901
brian-250 <brianmilian@csu.fullerton.edu>
2022-12-01 15:50:38 -0800

    started to write message functions.

--- part-2/make_message_functions.cc
+++ part-2/make_message_functions.cc
@@ -7,6 +7,9 @@
 bool HasMatchingFileExtension(const std::string& file_name,
                               const std::string& extension) {
   // TODO: Copy and paste the body of the function from Lab 10.
+  return ((file_name.size() >= extension.size()) &&
+          (file_name.compare(file_name.size() - extension.size(),
+                             extension.size(), extension) == 0));
   return false;
 }
 
@@ -25,14 +28,18 @@ RandomNumberGenerator rng_coin_flip{-1, 1, rng_seed};
 
 // Returns a random double between o and 1
 double RandomDouble01() {
+  // RandomNumberGenerator rng = RandomNumberGenerator(1.0, 10.0);
+  //int random_number = int(rng.next());
   // TODO: Using rng_01, call next() and return the next random number.
-  return 0;
+  double random_number1 = rng_01.next();
+  return random_number1;
 }
 
 // Returns a random double between -1 and 1
 double RandomDouble11() {
   // TODO: Using rng_11, call next() and return the next random number.
-  return 0;
+  double random_number11 = rng_11.next();
+  return random_number11;
 }
 
 /// Return a True or False depending on a random value.
@@ -40,5 +47,9 @@ double RandomDouble11() {
 bool CoinFlip() {
   // TODO: Using rng_coin_flip, call next() and return true if the randome
   // number is greater than 0.0 and false otherwise.
+  double rng_coin = rng_coin_flip.next();
+  if (rng_coin < 0.0){
+  return true;
+}
   return false;
 }
